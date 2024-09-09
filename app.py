813b48155b41d979eecf2e83fcbbbd21dfa9e9e3
O4FDev <luke@tekodot.com>
2024-09-09 13:46:39 +0100

    temp: intentionally break things

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from trywebscraping import Fetch
 
-hn = Fetch("https://news.ycombinator.com")
+hn = Fetch("https://this-is-an-invalid-site.com")
 articles = hn.query("tr.athing").extract({
     "rank": "span.rank",
     "title": "td.title a",
